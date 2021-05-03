7ffbb31520bbe2ac3755c65f8d73c10bf15db167
Nicolò Coltro <38473309+NiColtro@users.noreply.github.com>
2021-05-03 09:23:53 +0200

    Interfaccia Piante
    Update con compare

--- InterfacciaPiante.cs
+++ InterfacciaPiante.cs
@@ -16,7 +16,7 @@ namespace EsercizioInterfaccia {
         }
     }
     
-    class Esterno : Pianta, IPrezzo {
+    class Esterno : Pianta, IPrezzo, IComparable {
         
         public int Altezza { get; set; }
 
@@ -27,9 +27,27 @@ namespace EsercizioInterfaccia {
         public double CalcolaPrezzo() {
             return 0.5 * Altezza;
         }
+        
+        public int CompareTo(Object o) {
+            
+            double prezzo;
+            if (o.GetType() == typeof(Interno))
+                prezzo = (o as Interno).CalcolaPrezzo();
+            else
+                prezzo = (o as Esterno).CalcolaPrezzo();
+            
+            if (CalcolaPrezzo() == prezzo)
+                return Nome.CompareTo((o as Pianta).Nome);
+
+            return CalcolaPrezzo().CompareTo(prezzo);
+        }
+
+        public override string ToString() {
+            return "Pianta da Esterno\nNome: " + Nome + "\nPrezzo: " + CalcolaPrezzo() + "\n\n";
+        }
     }
     
-    class Interno : Pianta, IPrezzo {
+    class Interno : Pianta, IPrezzo, IComparable {
         
         public bool Fiori { get; set; }
 
@@ -42,14 +60,36 @@ namespace EsercizioInterfaccia {
                 return 10;
             return 5;
         }
+        
+        public int CompareTo(Object o) {
+            
+            double prezzo;
+            if (o.GetType() == typeof(Interno))
+                prezzo = (o as Interno).CalcolaPrezzo();
+            else
+                prezzo = (o as Esterno).CalcolaPrezzo();
+            
+            if (CalcolaPrezzo() == prezzo)
+                return Nome.CompareTo((o as Pianta).Nome);
+
+            return CalcolaPrezzo().CompareTo(prezzo);
+        }
+        
+        public override string ToString() {
+            return "Pianta da Interno\nNome: " + Nome + "\nPrezzo: " + CalcolaPrezzo() + "\n\n";
+        }
     }
     
     class Program {
         static void Main(string[] args) {
             List<Pianta> piante = new List<Pianta>();
             
-            // Add lista
+            piante.Add(new Interno("PiantaA", true));
+            piante.Add(new Esterno("PiantaB", 100000));
             
+            piante.Sort();
+            piante.ForEach(Console.WriteLine);
+
         }
     }
 }
